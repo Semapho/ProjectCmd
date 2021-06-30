a3051bd6919a4564af90e4dc1028d85c8fa988ac
Semapho <semaphores@qq.com>
2021-06-30 21:22:22 +0800

    1.Fix

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-cd /mnt/grey/
-nohup java -jar ./grey.jar&
+LogNameDATE=`date '+%Y_%m_%d'`
+LogFile=/mnt/grey/logrey/logrey_$LogNameDATE.log
+nohup java -jar /mnt/grey/grey.jar > $LogFile 2>&1
