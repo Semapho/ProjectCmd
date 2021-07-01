4a76d7194d09ee2098210cd1b91e0e02cd94b34c
Semapho <semaphores@qq.com>
2021-07-01 22:05:54 +0800

    1.Fix

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-ps -ef | grep grey| grep -v grep | awk '{print $2}' | xargs kill -9
 LogNameDATE=`date '+%Y_%m_%d'`
 LogFile=/mnt/grey/logrey/logrey_$LogNameDATE.log
 nohup java -jar /mnt/grey/grey.jar > $LogFile 2>&1
